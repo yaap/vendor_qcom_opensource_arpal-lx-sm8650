f0ed85d2b17a7b3d9c3978f28d9f2e59c93f21f2
Revanth Rajashekar <quic_rrajashe@quicinc.com>
2023-03-20 10:56:59 -0700

    pal: In context manager, handle back-to-back register requests
    The ssc_qsh_audio app fails to run the UPD/SensorPCMData usecases
    when two consecutive process register requests are made to the
    context manager.
    This is now handled by neglecting the second register request
    for non-ACD usecases.
    
    Update file's copyright year.
    
    Change-Id: Ic3ef5d66aef5d1378647858d175781956cec5296

--- context_manager/src/ContextManager.cpp
+++ context_manager/src/ContextManager.cpp
@@ -27,7 +27,7 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  * Changes from Qualcomm Innovation Center are provided under the following license:
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted (subject to the limitations in the
@@ -126,6 +126,11 @@ int32_t ContextManager::process_register_request(uint32_t see_id, uint32_t useca
 
         seeclient->Usecase_Add(usecase_id, uc);
     } else {
+
+        if (usecase_id != ASPS_USECASE_ID_ACD) {
+            goto exit;
+        }
+
         /* ASPS can send a request for an already running usecase with updated context ids lists. Configure()
          * to send the new list to the stream.
          */

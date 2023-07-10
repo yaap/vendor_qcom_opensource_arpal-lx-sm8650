bc86b774e2d6e6ad144344667e8f190e03046b56
Anand Mohan <quic_anamoh@quicinc.com>
2023-07-10 20:00:56 +0530

    pal: pass valid pal_device structure to streamDevSwitch function
    Passing address of a locally scoped pal_device structure to other
    functions will lead to garbage value when dereferenced as the
    adrress will not be valid outside the scope.
    
    Fixed this by declaring the structure in the main scope of the
    function.
    
    Change-Id: I2e1ea6f0fd22555337f9c51b589e3d898882c9b0

--- stream/src/Stream.cpp
+++ stream/src/Stream.cpp
@@ -1589,6 +1589,7 @@ int32_t Stream::switchDevice(Stream* streamHandle, uint32_t numDev, struct pal_d
     char CurrentSndDeviceName[DEVICE_NAME_MAX_SIZE] = {0};
     std::vector <Stream *> streamsToSwitch;
     struct pal_device streamDevAttr;
+    struct pal_device sco_Dattr = {};
     std::vector <Stream*>::iterator sIter;
     bool has_out_device = false, has_in_device = false;
     std::vector <std::shared_ptr<Device>>::iterator dIter;
@@ -1743,9 +1744,10 @@ int32_t Stream::switchDevice(Stream* streamHandle, uint32_t numDev, struct pal_d
             newBtDevId = newDevices[i].id;
             dev = Device::getInstance(&newDevices[i], rm);
             if (!dev) {
-                status = -ENODEV;
                 PAL_ERR(LOG_TAG, "failed to get a2dp/ble device object");
-                goto done;
+                mStreamMutex.unlock();
+                rm->unlockActiveStream();
+                return -ENODEV;
             }
             dev->getDeviceParameter(PAL_PARAM_ID_BT_A2DP_SUSPENDED,
                 (void**)&param_bt_a2dp);
@@ -1789,7 +1791,8 @@ int32_t Stream::switchDevice(Stream* streamHandle, uint32_t numDev, struct pal_d
         if (!dev) {
             PAL_ERR(LOG_TAG, "No device instance found");
             mStreamMutex.unlock();
-            return -EINVAL;
+            rm->unlockActiveStream();
+            return -ENODEV;
         }
         dev->insertStreamDeviceAttr(&newDevices[i], streamHandle);
         mPalDevices.push_back(dev);
@@ -1874,7 +1877,6 @@ int32_t Stream::switchDevice(Stream* streamHandle, uint32_t numDev, struct pal_d
                 (strAttr.type == PAL_STREAM_VOIP_TX &&
                     newDeviceId == PAL_DEVICE_IN_BLUETOOTH_SCO_HEADSET &&
                     rm->isDeviceActive(PAL_DEVICE_OUT_BLUETOOTH_SCO)))) {
-                struct pal_device sco_Dattr = {};
                 std::shared_ptr<Device> scoDev = nullptr;
                 std::vector <Stream*> activeStreams;
                 if (newDeviceId == PAL_DEVICE_OUT_BLUETOOTH_SCO) {
@@ -1888,7 +1890,7 @@ int32_t Stream::switchDevice(Stream* streamHandle, uint32_t numDev, struct pal_d
                     PAL_ERR(LOG_TAG, "getDeviceConfig for bt-sco failed");
                     mStreamMutex.unlock();
                     rm->unlockActiveStream();
-                    return 0;
+                    return status;
                 }
 
                 rm->getActiveStream_l(activeStreams, scoDev);
@@ -2023,7 +2025,6 @@ done:
         if (!volume) {
             PAL_ERR(LOG_TAG, "pal_volume_data memory allocation failure");
             mStreamMutex.unlock();
-            rm->unlockActiveStream();
             return -ENOMEM;
         }
         status = streamHandle->getVolumeData(volume);

31c2335f7b087c49feee7486db9020080b2903e4
Prasad Kumpatla <quic_pkumpatl@quicinc.com>
2023-08-16 14:50:09 +0530

    pal: add soundcard name support for cliffs
    add soundcard name support for cliffs.
    
    Change-Id: I9e74ed94572cab9bdb74caa49ea70f4e00eeee9e

--- resource_manager/src/ResourceManager.cpp
+++ resource_manager/src/ResourceManager.cpp
@@ -1570,6 +1570,7 @@ int ResourceManager::init_audio()
                     strstr(snd_card_name, "waipio") ||
                     strstr(snd_card_name, "kalama") ||
                     strstr(snd_card_name, "pineapple") ||
+                    strstr(snd_card_name, "cliffs") ||
                     strstr(snd_card_name, "anorak") ||
                     strstr(snd_card_name, "diwali") ||
                     strstr(snd_card_name, "bengal") ||
